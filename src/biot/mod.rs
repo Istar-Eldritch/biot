d2038dd9a4921b69e3c21e093531ec8d5ac6340a
Ruben Paz <me@ruben.io>
2017-06-10 18:08:20 +0100

    Add motif enumeration

--- src/biot/mod.rs
+++ src/biot/mod.rs
@@ -1,9 +1,11 @@
 mod pattern_count;
 mod frequent_words;
 mod skew;
+mod motifs;
 mod utils;
 
 pub use self::pattern_count::*;
 pub use self::frequent_words::*;
 pub use self::skew::*;
+pub use self::motifs::*;
 pub use self::utils::*;
